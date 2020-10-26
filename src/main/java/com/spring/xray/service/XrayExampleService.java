4316b0eb824f4acc05c1f273510e5a9aa0b47ea4
nara0617 <nara0617@gmail.com>
2020-10-26 22:30:57 +0900

    test

--- src/main/java/com/spring/xray/service/XrayExampleService.java
+++ src/main/java/com/spring/xray/service/XrayExampleService.java
@@ -1,15 +1,19 @@
 package com.spring.xray.service;
 
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
+import com.amazonaws.xray.proxies.apache.http.HttpClientBuilder;
 import com.amazonaws.xray.spring.aop.XRayEnabled;
 
 @Service("xrayExampleService")
@@ -20,24 +24,42 @@ public class XrayExampleService {
 
 	@Autowired
 	private RestTemplate restTemplate;
+
+//	@Bean
+//	public RestTemplate getRestTemplate() {
+//		return new RestTemplate();
+//	}
 	
 	@Bean
 	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+		return new RestTemplate(clientHttpRequestFactory());
 	}
-	
+
+	private HttpComponentsClientHttpRequestFactory clientHttpRequestFactory() {
+		PoolingHttpClientConnectionManager manager = new PoolingHttpClientConnectionManager();
+		manager.setDefaultMaxPerRoute(500);
+		manager.setMaxTotal(50);
+
+		HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+		CloseableHttpClient httpClient = HttpClientBuilder.create().setConnectionManager(manager).build();
+		factory.setHttpClient(httpClient);
+		return factory;
+	}
+
 	public String xrayDemoServiceMethod(String response) {
 		logger.info("sleuth xray Demo : ################################################### " + response);
 		return response;
 	}
 
 	public String xrayServiceMethod() {
-		String response = restTemplate.exchange("http://springboot2.default:8080/xraydemo", HttpMethod.GET, null, new ParameterizedTypeReference<String>() {
-		}).getBody();
+		String response = restTemplate.exchange("http://springboot2.default:8080/xraydemo", HttpMethod.GET, null,
+				new ParameterizedTypeReference<String>() {
+				}).getBody();
 
 		try {
-			String exceptionrsp = restTemplate.exchange("http://springboot2.default:8080/exception", HttpMethod.GET, null, new ParameterizedTypeReference<String>() {
-			}).getBody();
+			String exceptionrsp = restTemplate.exchange("http://springboot2.default:8080/exception", HttpMethod.GET,
+					null, new ParameterizedTypeReference<String>() {
+					}).getBody();
 			response = response + " ================= " + exceptionrsp;
 		} catch (Exception e) {
 			e.printStackTrace();
